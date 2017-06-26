c7c5edfd7de3b8fe5f7078dbde70efe8fcd77033
Diego Baño <diego.bano@ug.uchile.cl>
2017-06-26 16:53:46 -0400

    Fixed user elimination

--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
@@ -119,5 +119,6 @@ def delete_user(request):
 @login_required(login_url='/account/login')
 def confirm_deleted(request):
     request.user.delete()
+    logout(request)
     messages.add_message(request, messages.SUCCESS, "Usuario eliminado exitosamente")
     return render(request, 'account/deleted_confirmation.html')
