4be5c07003c022cb663884c1c0a5bd86f5ae32db
elixs <elixs.777@gmail.com>
2017-06-26 10:49:01 -0400

    dish delete working

--- showcase/urls.py
+++ showcase/urls.py
@@ -1,15 +1,22 @@
 from django.conf.urls import url
 from showcase import views
-from showcase.views import SellerDetailView, DishCreateView, DishUpdateView
+from showcase.views import SellerDetailView, DishCreateView, DishUpdateView, DishDeleteView
 
 app_name = 'showcase'
 
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', SellerDetailView.as_view(), name='seller_detail', ),
-    url(r'^(?P<pk>[0-9]+)/dish_create/$', DishCreateView.as_view(), name='dish_create', ),
-    url(r'^(?P<pk>[0-9]+)/dish_update/(?P<dish_id>[0-9]+)/$', DishUpdateView.as_view(), name='dish_update', ),
-    url(r'^statistics/$', views.statistics, name='statistics'),
 
+    # /showcase/<seller_id>/dish_create/<dish_id>/
+    url(r'^(?P<pk>[0-9]+)/dish_create/$', DishCreateView.as_view(), name='dish_create', ),
+
+    # /showcase/<seller_id>/dish_update/<dish_id>/
+    url(r'^(?P<pk>[0-9]+)/dish_update/(?P<dish_id>[0-9]+)/$', DishUpdateView.as_view(), name='dish_update', ),
+
+    # /showcase/<seller_id>/dish_delete/<dish_id>/delete
+    url(r'^(?P<pk>[0-9]+)/dish_delete/(?P<dish_id>[0-9]+)$', DishDeleteView.as_view(), name='dish_delete'),
+
+    url(r'^statistics/$', views.statistics, name='statistics'),
 
     # /showcase/<seller_id>/favorite
     # url(r'^(?P<seller_id>[0-9]+)/favorite/$', views.favorite_seller, name='favorite_seller'),
